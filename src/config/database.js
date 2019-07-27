bf3bb5ecdc23548fe4c868b5f4b21e0367f39dba
Anderson Alvarez V <andersonav91@gmail.com>
2019-07-26 19:25:17 -0500

    * Archivo y carpeta de archivos subidos

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.set('useFindAndModify', false);
-mongoose.connect((process.env.MONGODB_URI ? process.env.MONGODB_URI : 'mongodb://localhost/tdea'), {
+mongoose.connect((process.env.MONGODB_URI ? process.env.MONGODB_URI : 'mongodb://localhost/curso'), {
   useCreateIndex: true,
   useNewUrlParser: true
 })
